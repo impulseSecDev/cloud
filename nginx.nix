086cffbf0f0d80ab8c4e42a8b796a876e5fef27e
tim <timothy.harris0187@gmail.com>
2026-06-21 13:09:57 -0400

    incorporated nginx hardening

--- nginx.nix
+++ nginx.nix
@@ -32,6 +32,29 @@
   services.nginx = {
     enable = true;
     virtualHosts = {
+      # Default Fallback for HTTP (Port 80)
+      "default-http-fallback" = {
+        default = true; # Catches all traffic not matched by other virtual hosts
+        listen = [ { addr = "100.64.0.15"; port = 80; } ];
+        extraConfig = ''
+          server_name _;
+          error_page 400 =444 /;
+          return 444;
+        '';
+      };
+
+      # Default Fallback for HTTPS (Port 443)
+      "default-https-fallback" = {
+        default = true; # Catches all traffic not matched by other virtual hosts
+        listen = [ { addr = "100.64.0.15"; port = 443; ssl = true; } ];
+        extraConfig = ''
+          server_name _;
+          ssl_reject_handshake on; # Reject non-SSL attempts
+          error_page 400 401 402 403 404 405 429 497 500 =444 /;
+          return 444;
+        '';
+      };
+
       "cloud.mesh.loranjennings.com" = {
         useACMEHost = "mesh.loranjennings.com";
         forceSSL = true;
